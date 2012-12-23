c8beee1249a6ba82e11883e1cf5503489d301b54
James E Keenan <jkeenan@cpan.org>
2012-12-23 12:31:48 -0500

    Touch-ups: Add MANIFEST.SKIP.  Get test plans correct. CPAN v0.03.

--- lib/Text/CSV/Hashify.pm
+++ lib/Text/CSV/Hashify.pm
@@ -7,7 +7,7 @@ use Text::CSV;
 BEGIN {
     use Exporter ();
     use vars qw($VERSION @ISA @EXPORT);
-    $VERSION     = '0.02';
+    $VERSION     = '0.03';
     @ISA         = qw(Exporter);
     @EXPORT      = qw( hashify );
 }
